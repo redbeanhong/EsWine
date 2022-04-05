726e2b12deae9deafe8c34f573dc6bfcd5bd50d2
redbeanhong <myself6331@gmail.com>
2022-04-05 12:15:35 +0800

    Feat: 新增 Tailwind@2 使用設定

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,19 @@
 <template lang="pug">
   #index
+    h1 hello
     Navbar
     Banner
     Product
     Footbar
 </template>
-
+<style lang="scss">
+h1 {
+  @apply text-5xl;
+}
+h2 {
+  @apply text-3xl;
+}
+</style>
 <script>
 import Navbar from '@/components/Navbar.vue'
 import Banner from '@/components/Banner.vue'
